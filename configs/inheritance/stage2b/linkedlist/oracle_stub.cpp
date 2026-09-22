d9725ca11ea6f9b4ff45a52e06cd82710efd4feb
wwb <1599441272@qq.com>
2026-09-22 11:30:03 +0800

    fix: correct linkedlist stage2b oracle

--- configs/inheritance/stage2b/linkedlist/oracle_stub.cpp
+++ configs/inheritance/stage2b/linkedlist/oracle_stub.cpp
@@ -1,17 +1,25 @@
-#include <cstdlib>
+#include <algorithm>
 
 void process_top_hls(int n, int *input, int *output, int *fallback) {
     int values[32];
-    for (int i = 0; i < n; ++i) values[i] = input[i];
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if (values[j] < values[i]) {
-                int temp = values[i];
-                values[i] = values[j];
-                values[j] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < n; ++i) output[i] = values[i];
+    for (int i = 0; i < n; ++i) values[i] = input[n - 1 - i];
+    int cursor = 0;
+    for (int i = 0; i < n; ++i) output[cursor++] = values[i];
+    output[cursor++] = -1;
+
+    for (int i = n; i > 4; --i) values[i] = values[i - 1];
+    values[4] = 2000;
+    ++n;
+    for (int i = 0; i < n; ++i) output[cursor++] = values[i];
+    output[cursor++] = -1;
+
+    for (int i = 3; i < n - 1; ++i) values[i] = values[i + 1];
+    --n;
+    std::sort(values, values + n);
+    for (int i = 0; i < n; ++i) output[cursor++] = values[i];
+    output[cursor++] = -1;
+
+    for (int i = 0; i < n; ++i) output[cursor++] = values[n - 1 - i];
+    output[cursor++] = -1;
     *fallback = 0;
 }
